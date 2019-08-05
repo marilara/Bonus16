196ebea02b8b4f1bec526131d7fbd9c5480128da
maricela <laram@mail.gvsu.edu>
2019-08-05 17:49:57 -0400

    first commit

---
+++ Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Bonus_16
+{
+    class Car
+    {
+
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        public Car()
+        {
+            this.make = "Chevrolet";
+            this.model = "Traverse";
+            this.year = 2018;
+            this.price = 42000;
+        }
+        
+        public string Make { get { return make; } }
+        public string Model { get { return model; } }
+        public int Year {  get { return year; } }
+        public double Price { get { return price; } }
+
+        public Car(string make, string model, int year, double price)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+
+            return $"{Make,-15} {Model,-15} {Year,-15} {Price.ToString("C", CultureInfo.CurrentCulture),-15}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Bonus_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus Motors admin console!");
+            Console.WriteLine();
+            Console.Write("How many cars are you entering: ");
+            Console.WriteLine();
+            int numOfCars = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+            string Make = "";
+            string Model = "";
+            int Year = 0;
+            double Price = 0;
+
+            for (int i = 1; i <= numOfCars; i++)
+            {
+                Console.Write($"Enter Car #{i} Make: ");
+                Console.WriteLine();
+                Make = Console.ReadLine();
+                Console.Write($"Enter Car #{i} Model: ");
+                Console.WriteLine();
+                Model = Console.ReadLine();
+                Console.Write($"Enter Car #{i} Year: ");
+                Console.WriteLine();
+                Year = int.Parse(Console.ReadLine());
+                Console.Write($"Enter Car #{i} Price: ");
+                Console.WriteLine();
+                Price = Convert.ToDouble(Console.ReadLine());
+
+                cars.Add(new Car(Make, Model, Year, Price));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Current Inventory");
+            Console.WriteLine();
+            foreach (var c in cars)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+    }
+}
